6bf26bee97a9c53ce32ecf283525922e9c5a0e49
Shihab Sharar <shihabsharar90@gmail.com>
2021-11-28 15:54:25 +0600

    Updated

--- Products/forms.py
+++ Products/forms.py
@@ -1,6 +1,4 @@
 from django import forms
-from django.db import models
-from django.forms import fields
 
 from .models import Product
 
@@ -12,3 +10,8 @@ class ProductForm(forms.ModelForm):
             'description',
             'price'
         ]
+
+class RawProductForm(forms.Form):
+    title       =forms.CharField()
+    description =forms.CharField()
+    price       =forms.DecimalField()
--- Products/views.py
+++ Products/views.py
@@ -1,8 +1,51 @@
 from django.shortcuts import render
 
-from .forms import ProductForm
+from .forms import ProductForm, RawProductForm
 
 from .models import Product
+
+def product_create_view(request):
+    my_form = RawProductForm()
+    if request.method =="POST":
+        my_form = RawProductForm(request.POST)
+        if my_form.is_valid():
+            # now your data is good
+            print(my_form.cleaned_data)
+            Product.objects.create(**my_form.cleaned_data)
+        else:
+            print(my_form.errors)
+    context = {
+        "form": my_form
+    }
+    return render(request, "products/product_create.html", context)
+
+'''def product_create_view(request):
+    #print(request.GET)
+    #print(request.POST)
+    if request.method == "POST":
+        my_new_title = request.POST.get('title')
+        print(my_new_title)
+        # Product.objects.create(title=my_new_title)
+    context = {}
+    return render(request, "products/product_create.html", context)
+'''
+'''def product_create_view(request):
+    form = ProductForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        form = ProductForm()
+
+    context = {
+        'form': form
+    }
+    return render(request, "products/product_create.html", context)'''
+
+def product_detail_view(request):
+    obj = Product.objects.get(id=1)
+    context = {
+        'object': obj
+    }
+    return render(request, "products/product_detail.html", context)
 
 def product_create_view(request):
     form = ProductForm(request.POST or None)
@@ -14,10 +57,3 @@ def product_create_view(request):
         'form': form
     }
     return render(request, "products/product_create.html", context)
-
-def product_detail_view(request):
-    obj = Product.objects.get(id=1)
-    context = {
-        'object': obj
-    }
-    return render(request, "products/product_detail.html", context)
